3b50523b6e855f39161bbe01ec355a9035c4e1b8
Sean Bannon <seannbannon@gmail.com>
2023-01-31 09:10:25 -0600

    adding input to app js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import TodoList from "./TodoList";
 
 function App() {
   return (
+    // wrapped in empty element because you cant return more than one thing-- and here we aree returning 2 things
+    <>
     <TodoList />
+    <input type = "text" />
+    </>
   )
 };
 
