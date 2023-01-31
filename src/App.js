3eb2a4df27e87c18c11c47bf166d3ddc40fa2ec6
Sean Bannon <seannbannon@gmail.com>
2023-01-31 10:45:17 -0600

    deleting useState array object for now cuz we want to start with no todos obviously

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: "Todo 1", complete: false  }])
+  const [todos, setTodos] = useState([])
   return (
     // wrapped in empty element because you cant return more than one thing-- and here we aree returning 2 things
     <>
