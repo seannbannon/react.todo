9ef4bdbca7e0ac1a93e80950ccf0cd914b1c8d8d
Sean Bannon <seannbannon@gmail.com>
2023-01-31 10:44:12 -0600

    adding checkbox input type and hooking it up to complete status in useState array object

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Todo({ todo }) {
     return (
         <div>
-            {todo.name}
+            <label>
+                <input type="checkbox" checked= {todo.complete} />
+                {todo.name}
+            </label>
+            
         </div>
     )
 }
